c158e4ce6ac3e9eb1f9a7faa9202cf4d6ad4e6d8
Dave Pearson <davep@davep.org>
2017-11-20 10:17:15 +0000

    v1.4

--- nuke-buffers.el
+++ nuke-buffers.el
@@ -2,7 +2,7 @@
 ;; Copyright 2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.3
+;; Version: 1.4
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/nuke-buffers.el
 ;; Package-Requires: (emacs "25.1")
